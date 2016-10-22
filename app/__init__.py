0f74c23eb606eca0ff1bfee5bf198ad2939d5c8d
Gacheru <gacheruevans0@gmail.com>
2016-10-22 15:01:36 +0300

    Updated function Dashboard in init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 # Import flask and  SQLAlchemy template operators
 from jinja2 import Environment, PackageLoader
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, flash, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # Define the WSGI (Web server Gateway Interface) application object
 # Flask uses this argument to determine the root path of the application so
 # that it later can find resource files relative to the location of the application
+
+
 app = Flask(__name__)
-oxygen = Environment(loader=PackageLoader('app', 'templates'))
+env = Environment(loader=PackageLoader('app', 'templates'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # Configurations
@@ -20,28 +22,33 @@ db = SQLAlchemy(app)
 
 # @ decorator register functions as handlers for an event
 @app.route('/')
-@app.route('/<username>/')
-@app.route('/signin/')
+#@app.route('/<username>')
 def index(username=None):
     return render_template('index.html', user=username)
 
 
-
-@app.route('/signup/')
+@app.route('/signup', methods=['GET', 'POST'])
 def Registration():
-    return render_template('auth/signup.html'), 404
+    return render_template('auth/signup.html')
 
 
-@app.route('/dashboard/<int:id>/<int:role>/<username>')
-def Dashboard(id, role,username):
-    books = ['Book1', 'Book2', 'Book3', 'Book4', 'Book5', 'Book6', 'Book7', 'Book8']
-    return render_template('dashboard.html', id=id, role=role,username=username, books=books), 404
+@app.route('/dashboard', methods=['GET', 'POST'])
+def Dashboard(books=None, users=None):
+    from mod_auth.models import Books,User
+    books = Books.query.all() or None
+    users = User.query.all() or None
+    return render_template('dashboard.html', books=books, users=users)
 
 
-# Sample HTTP error handling
+# HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 # Import a module / component using its blueprint handler variable (mod_auth)
